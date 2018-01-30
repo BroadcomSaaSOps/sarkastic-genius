d1a3f7931df6172cd95a72bb1b1a1c98c0641d58
Matt King <matt.king@ca.com>
2018-01-30 08:27:00 -0500

    Add version tag to provider.tf to prevent updates from breaking the pipeline

--- roles/os-install/templates/vsphere-provider.tf
+++ roles/os-install/templates/vsphere-provider.tf
@@ -3,4 +3,5 @@ provider "vsphere" {
   password             = "{{ tf_vsphere_password }}"
   vsphere_server       = "{{ tf_vsphere_server }}"
   allow_unverified_ssl = "{{ tf_vsphere_allow_unverified_ssl }}"
+  version              = "~> 0.4"
 }
